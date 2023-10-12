81882869ba1247cde47bed5eb8a0cde5d4db077f
Michael Schernthaner <michaelschernthaner@MBP-von-Michael.localdomain>
2023-10-12 15:45:24 +0200

    ArrMethods

--- play.js
+++ play.js
@@ -1,10 +1,21 @@
-let test = 10
-function first() {
-    test += 5
-    console.log(test)
-    if (true) {
-        test +=5
-        console.log(test)
-    }
-}
-console.log(test)
+
+function double(arr) {
+    return arr.map(function(val) {
+      return val * 2;
+    });
+  }
+
+const double2 = arr => arr.map(val => val * 2);
+
+
+function squareAndFindEvens(numbers){
+    var squares = numbers.map(function(num){
+      return num ** 2;
+    });
+    var evens = squares.filter(function(square){
+      return square % 2 === 0;
+    });
+    return evens;
+  }
+
+const squareAndFindEvens2 = numbers => numbers.map(num => num ** 2).filter(square => square % 2 ===0)
